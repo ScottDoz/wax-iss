5a5264dc56e082a9c22cffdb6df5376be3fae820
ScottDoz <s.dorrington@unswalumni.com>
2026-06-09 18:51:04 -0400

    Removed old motor test scripts

--- tests/motor_old/2_MOTOR_encoder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Testing Motor"
-sudo python ~/wax-iss/tests/motor/test_motorencodersignal.py
-sleep 15
--- tests/motor_old/2_MOTOR_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Testing Motor"
-sudo python ~/wax-iss/tests/motor/test_motor.py
-sleep 15
