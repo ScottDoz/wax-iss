51dfb97984a67c2bef01d759b7d78374894cb25e
ScottDoz <s.dorrington@unswalumni.com>
2026-06-16 19:26:54 -0400

    Updated fluid rotation experiment

---
+++ Scripts/Experiments/FLUID_run_fluid_rot_exp.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Variables
+label="TMI_Tests"
+accel=20 # Acceleration/deceleration value (rev/s/s)
+rpm=100
+setpoint=20
+sudo_pass="raspberry"
+
+# Start logging
+echo "Starting fluid rotation experiemnt"
+#sudo python ~/wax-iss/wax/melt_client.py start_log_exp $label,"Fluid","$rpm","$setpoint" # Lights, camera, data log
+sudo python ~/wax-iss/wax/melt_client.py start_log_preview_exp $label,"Fluid","$rpm","$setpoint" # Lights, camera, data log
+sleep 3
+
+# Turn heater off
+sudo python ~/wax-iss/wax/melt_client.py set_setpoint "$setpoint" # Change CAL temperature setpoint (turn off)
+sleep 1
+
+# Speed = 100 RPM ------------------------------------------------------
+
+RPM=100
+
+# Individual time-based st-curve profiles
+
+# Profile A-1
+T13=2.5
+T2=0.0
+echo "Profile A-1: T1=T3=$T13 s, T2=$T2 s"
+sudo python ~/wax-iss/wax/melt_client.py set_motor_mode_st_time_based "$T13","$T2" # Mode 2: St-curve time-based
+sleep 1
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed "$rpm" # Ramp up motors
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 5}")
+sleep "$SLEEP_TIME" # Wait until ramped up, hold for 5 sec
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed 0 # Stop rotation
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 10}")
+sleep "$SLEEP_TIME" # Wait until ramped down, hold for 20 sec
+
+
+# Profile A-1
+T13=5.0
+T2=0.0
+echo "Profile A-2: T1=T3=$T13 s, T2=$T2 s"
+sudo python ~/wax-iss/wax/melt_client.py set_motor_mode_st_time_based "$T13","$T2" # Mode 2: St-curve time-based
+sleep 1
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed "$rpm" # Ramp up motors
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 5}")
+sleep "$SLEEP_TIME" # Wait until ramped up, hold for 5 sec
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed 0 # Stop rotation
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 10}")
+sleep "$SLEEP_TIME" # Wait until ramped down, hold for 20 sec
+
+# Profile A-3
+T13=7.5
+T2=0.0
+echo "Profile A-3: T1=T3=$T13 s, T2=$T2 s"
+sudo python ~/wax-iss/wax/melt_client.py set_motor_mode_st_time_based "$T13","$T2" # Mode 2: St-curve time-based
+sleep 1
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed "$rpm" # Ramp up motors
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 5}")
+sleep "$SLEEP_TIME" # Wait until ramped up, hold for 5 sec
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed 0 # Stop rotation
+SLEEP_TIME=$(awk "BEGIN {print 2 * $T13 + $T2 + 10}")
+sleep "$SLEEP_TIME" # Wait until ramped down, hold for 20 sec
+
+
+# OLD: Constant accel ramp
+#sudo python ~/wax-iss/wax/melt_client.py motor_ramp_updown_const_accel "50" # Motion profile 50 RPM
+#sleep 1
+#sudo python ~/wax-iss/wax/melt_client.py motor_ramp_updown_const_accel "100" # Motion profile 100 RPM
+#sleep 1
+
+# Stop script
+echo "Stopping fluid rotation experiemnt"
+sudo python ~/wax-iss/wax/melt_client.py set_target_load_speed 0 # Stop rotation
+echo "Ramping down motor"
+sleep 1 # Sleep
+sudo python ~/wax-iss/wax/melt_client.py stop_log_exp # Stop data log, turn off lights, camera
+echo "Stopping lights, camera, data log"
+sleep 5
+echo "Fluid rotation complete"
