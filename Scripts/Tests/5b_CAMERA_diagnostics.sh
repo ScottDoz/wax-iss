0fd466ef6db1e9f048d1acd6dedcfc1f3fedcdbf
ScottDoz <s.dorrington@unswalumni.com>
2026-08-05 14:53:05 -0400

    Added camera diagnostics script

---
+++ Scripts/Tests/5b_CAMERA_diagnostics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+uname -r
+echo ">>lsmod | grep unicam"
+lsmod | grep unicam
+echo ""
+
+echo ">> ls -l /dev/media*"
+ls -l /dev/media*
+echo ""
+
+
+echo ">>dmesg | grep -Ei camera|csi|unicam"
+dmesg | grep -Ei "camera|csi|unicam"
+echo ""
+
+echo ">>libcamera-hello --qt-preview"
+libcamera-hello --qt-preview
+
+
+# Keep terminal open
+exec bash
