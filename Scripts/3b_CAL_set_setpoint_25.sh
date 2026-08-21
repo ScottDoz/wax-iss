743f21fa1925576d5bca72bcb7bb3d4c60f739d3
ScottDoz <s.dorrington@unswalumni.com>
2026-08-21 10:26:00 -0400

    Added script setpoint 25C

---
+++ Scripts/3b_CAL_set_setpoint_25.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Activate venv
+source ~/Documents/myenv/bin/activate
+
+echo "Change Temp Setpoint"
+sudo python ~/wax-iss/wax/melt_client.py set_setpoint 25
+sleep 5 
+
+# Keep terminal open on error
+#exec bash
+if [ $? -ne 0 ]; then
+	echo "***********************"
+	echo "!!! Error Detected!!!!"
+	read -p "Press [ENTER] to close the terminal..."
+	exit 1
+else
+	exit 0
+fi
